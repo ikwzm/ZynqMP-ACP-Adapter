98c7e66c01b705d36b5d206e19ee8a1f9943082f
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2019-10-28 18:14:13 +0900

    [add] sim/vivado/

---
+++ sim/vivado/add_sources.tcl
@@ -0,0 +1,7 @@
+add_vhdl_file sources_1 PIPEWORK ../../PipeWork/src/components/components.vhd
+add_vhdl_file sources_1 PIPEWORK ../../PipeWork/src/components/queue_receiver.vhd
+add_vhdl_file sources_1 PIPEWORK ../../PipeWork/src/components/queue_register.vhd
+add_vhdl_file sources_1 WORK ../../src/main/vhdl/zynqmp_acp_response_queue.vhd
+add_vhdl_file sources_1 WORK ../../src/main/vhdl/zynqmp_acp_read_adapter.vhd
+add_vhdl_file sources_1 WORK ../../src/main/vhdl/zynqmp_acp_write_adapter.vhd
+add_vhdl_file sources_1 WORK ../../src/main/vhdl/zynqmp_acp_adapter.vhd
